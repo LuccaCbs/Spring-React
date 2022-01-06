32560e70ee755ba3d93b793c386dd2bfa11288d4
Lucca <lucca.vergara2.0@gmail.com>
2022-01-06 04:26:35 -0300

    Spring-React v.1

---
+++ Spring Rest Api/src/main/java/SpringReact/App/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package SpringReact.App.controller;
+
+import SpringReact.App.model.Employee;
+import SpringReact.App.repository.EmployeeRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:3000/")
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    //get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+    
+    
+}
